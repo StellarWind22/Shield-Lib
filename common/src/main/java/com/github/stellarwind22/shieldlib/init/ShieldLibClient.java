834d5a5cab006cb9a5c80c95da20fc9aedc12d87
StellarWind22 <gannonburks45@gmail.com>
2025-08-12 13:38:12 -0500

    textures, first person models, & shield handling logic is broken, but it runs!

--- common/src/main/java/com/github/stellarwind22/shieldlib/init/ShieldLibClient.java
+++ common/src/main/java/com/github/stellarwind22/shieldlib/init/ShieldLibClient.java
@@ -1,14 +1,29 @@
 package com.github.stellarwind22.shieldlib.init;
 
+import com.github.stellarwind22.shieldlib.lib.render.ShieldLibModelRenderer;
+import com.github.stellarwind22.shieldlib.mixin.SpecialModelRenderersAccessor;
+import com.mojang.serialization.MapCodec;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.special.SpecialModelRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.ExtraCodecs;
 
 @Environment(EnvType.CLIENT)
 public class ShieldLibClient {
 
     public static boolean IS_FABRIC;
 
+    public static final ResourceLocation BANNER_SHIELD_MODEL_TYPE = ResourceLocation.fromNamespaceAndPath(ShieldLib.MOD_ID, "banner_shield");
+
     public static void init(boolean isFabric) {
         IS_FABRIC = isFabric;
+
+        ExtraCodecs.LateBoundIdMapper<ResourceLocation, MapCodec<? extends SpecialModelRenderer.Unbaked>> idMapper = SpecialModelRenderersAccessor.getIDMapper();
+
+        idMapper.put(
+                BANNER_SHIELD_MODEL_TYPE,
+                ShieldLibModelRenderer.Unbaked.CODEC
+        );
     }
 }
--- neoforge/src/main/java/com/github/stellarwind22/shieldlib/neoforge/init/ShieldLibNeoForge.java
+++ neoforge/src/main/java/com/github/stellarwind22/shieldlib/neoforge/init/ShieldLibNeoForge.java
@@ -1,9 +1,12 @@
 package com.github.stellarwind22.shieldlib.neoforge.init;
 
 import com.github.stellarwind22.shieldlib.init.ShieldLib;
+import com.github.stellarwind22.shieldlib.init.ShieldLibClient;
 import com.github.stellarwind22.shieldlib.test.ShieldLibTests;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.fml.loading.FMLLoader;
 
 @Mod(ShieldLib.MOD_ID)
 public final class ShieldLibNeoForge {
@@ -15,6 +18,10 @@ public final class ShieldLibNeoForge {
         if(!FMLEnvironment.production) {
             ShieldLibTests.init();
             ShieldLib.LOGGER.warn("TEST CODE IS CURRENTLY RUNNING!, IF YOU ARE NOT IN A DEV ENVIRONMENT THIS IS BAD!!");
+        }
+
+        if(FMLLoader.getDist() == Dist.CLIENT) {
+            ShieldLibClient.init(false);
         }
 
         ShieldLib.LOGGER.info("ShieldLib initialized!");
