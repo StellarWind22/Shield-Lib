7785416a0cc4c438ae69d68e7cabc68dfc5a954b
StellarWind22 <gannonburks45@gmail.com>
2025-08-24 12:22:36 -0500

    Still broken but fixed lang file.

--- testfabric/src/main/java/com/github/stellarwind22/testfabric/init/TestFabricClient.java
+++ testfabric/src/main/java/com/github/stellarwind22/testfabric/init/TestFabricClient.java
@@ -4,6 +4,7 @@ import com.github.stellarwind22.testfabric.ShieldLibTests;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TestFabricClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
         ShieldLibTests.initClient();
